a7495cbbdeb16739ef3a2fa20b35a53956e59d41
MrMauro <maurizio3324@gmail.com>
2026-06-19 01:17:11 -0500

    Método main para encender

---
+++ inventory-service/src/main/java/com/vitivinicolas/inventory/InventoryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.vitivinicolas.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] eloquence) {
+        SpringApplication.run(InventoryServiceApplication.class, eloquence);
+    }
+}
